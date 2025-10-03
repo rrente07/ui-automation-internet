21f19a43fc63a9b5ca365c07efd3d26d03352695
Rosa Rente <rrente07@gmail.com>
2025-10-03 16:39:00 -0400

    Work with Add amd Remove 0

---
+++ src/main/java/utils/Utility.java
@@ -0,0 +1,35 @@
+package utils;
+
+import org.openqa.selenium.WebElement;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.openqa.selenium.WebElement;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+    public class Utility {
+
+        public static void printElementInfo(String name, WebElement element) {
+            String marker = "------------------------------";
+            System.out.println(marker);
+            System.out.println("Element meta-data: " + name);
+            System.out.println(marker);
+            System.out.println("WebElement object:: " + element);
+            System.out.println("Outer HTML:: " + element.getAttribute("outerHTML"));
+            System.out.println("Inner HTML:: " + element.getAttribute("innerHTML"));
+            System.out.println("Text:: " + element.getText());
+            System.out.println("Value::" + element.getAttribute("value"));
+            System.out.println("Tag:: " + element.getTagName());
+            System.out.println("Displayed?:: " + element.isDisplayed());
+            System.out.println("Selected?:: " + element.isSelected());
+            System.out.println("Enabled?:: " + element.isEnabled());
+            System.out.println(marker);
+        }
+
+    }
--- src/test/java/test/AlertsTest.java
+++ src/test/java/test/AlertsTest.java
@@ -1,17 +1,16 @@
     package test;
 
-    import org.junit.jupiter.api.Assertions;
-    import org.junit.jupiter.api.BeforeAll;
-    import org.junit.jupiter.api.Test;
+    import lombok.extern.java.Log;
+    import org.junit.jupiter.api.*;
     import org.slf4j.Logger;
     import org.slf4j.LoggerFactory;
     import pages.AlertsPage;
     import pages.HomePage;
 
     public class AlertsTest extends BaseTest{
+    private static final Logger logger = LoggerFactory.getLogger(AlertsTest.class);
 
 
-        private static final Logger logger = LoggerFactory.getLogger(DropdownTest.class);
         static HomePage homePage;
         static AlertsPage alertsPage;
 
@@ -20,38 +19,49 @@
         static void setUp() {
             alertsPage = new AlertsPage();
             homePage = new HomePage(driver);
-
         }
 
+        @BeforeEach
+        void setUpEach() {
+            driver.get("https://the-internet.herokuapp.com/");
+            homePage = new HomePage(driver);
+        }
+
+
         @Test
+        @DisplayName("Verify that we click on the button JavaScript Alerts and display Click for JS Alert")
         public void testAcceptAlert(){
                 alertsPage = homePage.ClickOnAlertsJS();
-                System.out.println("Current URL: " + driver.getCurrentUrl());
-                System.out.println("Trying to accept alert...");
-                alertsPage.ClickAcceptAlertJS(); // ya incluye espera y aceptación
+                logger.info("Current URL: " + driver.getCurrentUrl());
+                logger.info("Trying to accept alert...");
+                alertsPage.ClickBtnAlertJS(); // that include wait and acceptation
             boolean isPresent = alertsPage.isElementVisibleAlert();
             Assertions.assertTrue(isPresent, "Result message not visible");
             String alertText = alertsPage.getMessageAlertText();
             Assertions.assertEquals("You successfully clicked an alert", alertText, "Failed");
             }
 
+
         @Test
+        @DisplayName("Verify that we click on the button JavaScript Alerts and display Click for JS Confirm")
         public void testAcceptAlertJSConfirm(){
-            alertsPage = homePage.ClickOnAlertsJSConfirm();
-            System.out.println("Current URL: " + driver.getCurrentUrl());
-            System.out.println("Trying to accept alert to Confirm...");
-            alertsPage.ClickAcceptAlertJSConfirm(); // ya incluye espera y aceptación
+            alertsPage = homePage.ClickOnAlertsJS();
+            logger.info("Current URL: " + driver.getCurrentUrl());
+            logger.info("Trying to accept alert to Confirm...");
+            alertsPage.ClickBtnAlertJSConfirm(); // ya incluye espera y aceptación
             boolean isPresent = alertsPage.isElementVisibleAlert();
             Assertions.assertTrue(isPresent, "Result message not visible");
             String alertText = alertsPage.getMessageAlertText();
             Assertions.assertEquals("You clicked: Ok", alertText, "Failed");
         }
 
+
         @Test
+        @DisplayName("Verify that we click on the button JavaScript Alerts and dismiss Click for JS Confirm")
         public void testDismissAlert(){
-            alertsPage = homePage.ClickOnAlertsJSConfirm();
-            System.out.println("Current URL: " + driver.getCurrentUrl());
-            System.out.println("Trying to accept alert to Confirm...");
+            alertsPage = homePage.ClickOnAlertsJS();
+            logger.info("Current URL: " + driver.getCurrentUrl());
+            logger.info("Trying to accept alert to Confirm...");
             alertsPage.ClickDismissAlertJSConfirm(); // ya incluye espera y aceptación
             boolean isPresent = alertsPage.isElementVisibleAlert();
             Assertions.assertTrue(isPresent, "Result message not visible");
@@ -61,4 +71,25 @@
 
         }
 
+
+        @Test
+        @DisplayName("Verify that we click on the button JavaScript Alerts and display Click for JS Prompt")
+        public void testAcceptAlertPrompt()
+        {
+            alertsPage = homePage.ClickOnAlertsJS();
+            logger.info("Current URL: " + driver.getCurrentUrl());
+            logger.info("Trying to Click for JS Prompt...");
+
+            alertsPage.triggerPromptAlertOnly();
+            String message = "test";
+            homePage.sendKeysToAlert(message);
+
+            boolean isPresent = alertsPage.isElementVisibleAlert();
+            Assertions.assertTrue(isPresent, "Result message not visible");
+
+            String alertText = alertsPage.getMessageAlertText();
+            Assertions.assertEquals("You entered: " + message,alertText , "Failed");
+            System.out.println("Result when we entered the test into the alert: " + message);
+
+        }
     }
